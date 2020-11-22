e5d57e7bb762f0f881a39e266bc5f9ffebae5810
rePakkun <cj.ilya@yandex.ru>
2020-11-22 14:27:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = "ilya";
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
