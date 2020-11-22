70a87fe2060fd41e994bc8110e7b8a6a87a26b20
rePakkun <cj.ilya@yandex.ru>
2020-11-22 16:38:57 +0300

    Updated

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,14 +3,17 @@
 namespace ConsoleApp1
 {
     class Program
-    {
-        /*
-        enum Senaphore 
+    {   
+        enum DayOfWeek 
         {
-            Red = 100,
-            Yellow = 200,
-            Green = 300
-        }*/
+            Monday = 1,
+            Tuesday,
+            Wendsday, 
+            Thursday,
+            Friday, 
+            Saturday,
+            Sunday
+        }
         static void Main(string[] args)
         {
              string MyName = "Ilya";
@@ -34,14 +37,18 @@ namespace ConsoleApp1
            Console.Write("Введите имя: ");
             string name = Console.ReadLine();
 
+            //пример явного преобразования с возможной потерей данных
             Console.Write("Введите возраст: ");
-            int age = Int32.Parse(Console.ReadLine());
-
+            int age = (byte) int.Parse(Console.ReadLine());
             Console.WriteLine($"Ваше имя {name}, ваш возраст: {age}");
 
             Console.Write("Введите дату рождения: ");
             string birthDate = Console.ReadLine();
             Console.WriteLine($"Ваша дата рождения {birthDate}");
+
+            Console.Write("Ваш любимый день недели?");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ваш любимый день это {day}");
 
 
             Console.ReadKey();
